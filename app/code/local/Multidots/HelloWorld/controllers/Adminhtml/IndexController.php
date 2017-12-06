78fcf7b2e0e8cec0b733bc7f54c3c413c31dcff6
Nimfus <nimfus@gmail.com>
2017-12-06 10:44:59 +0100

    test

---
+++ app/code/local/Multidots/HelloWorld/controllers/Adminhtml/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+class Multidots_HelloWorld_Adminhtml_IndexController extends Mage_Adminhtml_Controller_Action {
+
+    /**
+     * Admin controller index action
+     *
+     * @access public
+     * @return void
+     */
+
+    public function indexAction()
+    {
+        echo 'hello world in the admin side..!!!'. '';
+    }
+}
