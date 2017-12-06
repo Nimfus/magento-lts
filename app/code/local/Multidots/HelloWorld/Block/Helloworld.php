8ae456f10138d06dc0a1164dee532f1bcb60ed6d
Nimfus <nimfus@gmail.com>
2017-12-06 11:57:55 +0100

    test

--- app/code/local/Multidots/HelloWorld/Block/Helloworld.php
+++ app/code/local/Multidots/HelloWorld/Block/Helloworld.php
@@ -1,6 +1,9 @@
 <?php
 class Multidots_HelloWorld_Block_Helloworld extends Mage_Core_Block_Template
 {
+    /**
+     * @return string
+     */
     protected function _toHtml()
     {
         return "Hello World";
